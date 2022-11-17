55b9d0be39e7cbd085d75f267925380d76f7cf21
SardineSP <5039662@naver.com>
2022-11-17 15:16:27 +0900

    Update DimensionPractice.java

--- 04_Array/src/com/kh/dimension/DimensionPractice.java
+++ 04_Array/src/com/kh/dimension/DimensionPractice.java
@@ -119,7 +119,7 @@ public class DimensionPractice {
 		}
 	}
 	
-	public void practice6() {
+	public void practice6() { //이것도 약간 다른듯?
 		String [][] strArr = new String[][] {
 			{"이", "까", "왔", "앞", "힘"}, 
 			{"차", "지", "습", "으", "냅"}, 
@@ -135,7 +135,7 @@ public class DimensionPractice {
 		}
 	}
 	
-	public void practice7() { ////////
+	public void practice7() { //////// 이것도 약간 다르니 참고, 좀 더 이해 되도록 파악해보기
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("행의 크기 : ");
@@ -153,7 +153,11 @@ public class DimensionPractice {
 			arr[i] = new char[column]; 
 			for(int j = 0; j < column; j++) {
 				arr[i][j] = ch;
+				if(ch == 'z') { // 이처럼 범위 지정해줌
+					ch = 'a';
+				}else {
 				ch++;
+				}
 			}
 		}
 		for(int i = 0; i < row; i++) {
@@ -175,8 +179,8 @@ public class DimensionPractice {
 		System.out.println("== 1분단 ==");
 		for(int i = 0; i < desk1.length; i++) {
 			for(int j = 0; j < desk1[i].length; j++) {
-				desk1[i][j] = students[num];
-				num++;
+				desk1[i][j] = students[num++];
+				//num++;
 				System.out.print(desk1[i][j] + " ");
 			}
 			System.out.println();
@@ -186,15 +190,15 @@ public class DimensionPractice {
 		
 		for(int i = 0; i < desk2.length; i++) {
 			for(int j = 0; j < desk2[i].length; j++) {
-				desk2[i][j] = students[num];
-				num++;
+				desk2[i][j] = students[num++];
+				//num++;
 				System.out.print(desk2[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 	
-	public void practice9() {
+	public void practice9() { // 이것도 파일 꼭 확인하기
 		String [] students = {"강건강", "남나나", "도대담", "류라라", "문미미", "박보배", "송성실", "윤예의", "진재주", "차천축", "피풍표", "홍하하"};
 		
 		String [][] desk1 = new String[3][2];
@@ -212,6 +216,7 @@ public class DimensionPractice {
 		
 		String rl;
 		
+		//char [] line = {'첫', '두', '세'}
 		for(int i = 0; i < desk1.length; i++) {
 			for(int j = 0; j < desk1[i].length; j++) {
 				if(name.equals(desk1[i][j])) {
@@ -222,7 +227,8 @@ public class DimensionPractice {
 					}else {
 						rl = "세";
 					}
-					System.out.printf("검색하신 %s 학생은 1분단 %s 번째 줄 %s쪽에 있습니다.",name, rl, (j == 1? "오른" : "왼"));
+					System.out.printf("검색하신 %s 학생은 1분단 %s 번째 줄 %s쪽에 있습니다.",name, rl, (j == 1? "오른" : "왼")); 
+					// 위의 line[i] 활용도 가능
 					continue;
 				}
 			}
