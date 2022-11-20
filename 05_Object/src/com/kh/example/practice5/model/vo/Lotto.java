2b358c81897c69900a0f1b2b0336b5be7e3d98ab
SardineSP <5039662@naver.com>
2022-11-20 21:50:15 +0900

    Update Lotto.java

--- 05_Object/src/com/kh/example/practice5/model/vo/Lotto.java
+++ 05_Object/src/com/kh/example/practice5/model/vo/Lotto.java
@@ -13,37 +13,25 @@ public class Lotto {
 
 	}
 	
-	
 
 	public int[] getLotto() {
 		return lotto;
 	}
-
-
 
 	public void setLotto(int[] lotto) {
 		this.lotto = lotto;
 	}
 
 
-
 	public void information() { //중복 빼기
 		for(int i = 0; i < 6; i++) {
 			for(int j = 0; j < i; j++) {
 				if(lotto [i] == lotto[j]) {
-					Lotto();
+					lotto [i] = (int)((Math.random()*44)+ 1);;
 				}
 			}
-			
 			System.out.print(lotto[i] + " ");
-		
 		}
 	}
 
-
-
-	private void Lotto() {
-		// TODO Auto-generated method stub
-		
-	}
 }
