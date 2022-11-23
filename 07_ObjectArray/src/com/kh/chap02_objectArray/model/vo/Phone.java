a18283675d02968bddaea6c417a1b6390843171e
SardineSP <5039662@naver.com>
2022-11-23 10:47:40 +0900

    2022.11.23

--- 07_ObjectArray/src/com/kh/chap02_objectArray/model/vo/Phone.java
+++ 07_ObjectArray/src/com/kh/chap02_objectArray/model/vo/Phone.java
@@ -1,6 +1,6 @@
 package com.kh.chap02_objectArray.model.vo;
 
-public class Phone {
+public class Phone { //여기에 뭔가 문제가 있는듯?
 	
 	private String name;
 	private String series;
@@ -12,7 +12,10 @@ public class Phone {
 	}
 	
 	public Phone(String name, String series, String brand, int price) {
-		
+		this.name = name;
+		this.series = series;
+		this.brand = brand;
+		this.price = price;
 	}
 
 	public void setName(String name) {
@@ -39,19 +42,16 @@ public class Phone {
 		return series;
 	}
 
-
 	public String getBrand() {
 		return brand;
 	}
-
 
 	public int getPrice() {
 		return price;
 	}
 
-	
 	public String information() {
-		return "";
+		return name + " " + series + " " + brand + " " + price ;
 	}
 
 }
--- 07_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
+++ 07_ObjectArray/src/com/kh/chap02_objectArray/run/ObjectArrayRun.java
@@ -1,10 +1,29 @@
 package com.kh.chap02_objectArray.run;
 
+import com.kh.chap02_objectArray.model.vo.Phone;
+
 public class ObjectArrayRun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Phone[] arr = new Phone[3];
+		
+		arr[0] = new Phone();
+		arr[0].setName("아이폰");
+		arr[0].setSeries("12");
+		arr[0].setBrand("애플");
+		arr[0].setPrice(100_0000);
+		
+		arr[1] = new Phone("아이폰", "13", "애플", 150_0000); //이거 왜 null값이 나오지?
+		arr[2] = new Phone("갤럭시", "Z Flip 4", "삼성", 135_0000);
+		
+		int sum = 0;
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i].information());
+			sum += arr[i].getPrice();
+		}
+		
+		System.out.println("총 가격 : " + sum + "원");
+		System.out.println("평균 가격 : " + sum / arr.length + "원");
 	}
 
 }
