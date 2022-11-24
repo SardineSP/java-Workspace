cbddfbb23ef1a6c1d8966127a22cbc46009ac814
SardineSP <5039662@naver.com>
2022-11-24 15:18:10 +0900

    Update RectangleController.java

--- 08_Inheritance/src/com/kh/practice/point/controller/RectangleController.java
+++ 08_Inheritance/src/com/kh/practice/point/controller/RectangleController.java
@@ -6,7 +6,7 @@ public class RectangleController {
 	private Rectangle r = new Rectangle();
 	
 	public String calcArea(int x, int y, int height, int width) {
-		r = new Rectangle(x, y, height, width);
+		r = new Rectangle(x, y, height, width); // 이부분이 값 초기화? 이게 없어서 값이 대입이 안됐었음 ㅋㅋㅋ
 		return "면적 : " + r.toString() + " / " + (height*width);
 	}
 	
